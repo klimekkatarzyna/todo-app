ecd9ac4e400ad7cdec60de9d12113897a8bc6f91
Katarzyna Klimek <katarzynaklimek26@gmail.com>
2022-04-08 23:58:45 +0200

    MVC pattern for groups

--- server/src/routes/groups.ts
+++ server/src/routes/groups.ts
@@ -1,78 +1,16 @@
 import { createEditGroupSchema, CreateEditGroupType } from '@kkrawczyk/todo-common';
-import express, { Request, Response } from 'express';
-import { Group } from '../models/group';
-import { getSessionUserId } from '../utils/auth';
+import express from 'express';
+import { createGroup, editGroup, getGroups, removeGroup } from '../controllers/groups';
 import { validateBody } from '../utils/validation';
 
 const groups = express.Router();
 
-groups.post('/createGroup', validateBody<CreateEditGroupType>(createEditGroupSchema), async (req: Request, res: Response) => {
-	const userId = getSessionUserId(req);
+groups.post('/createGroup', validateBody<CreateEditGroupType>(createEditGroupSchema), createGroup);
 
-	const newGroup = new Group({
-		title: req.body.title,
-		themeColor: 'blue',
-		createdAt: Date.now(),
-		userId: userId,
-	});
+groups.get('/getGroups', getGroups);
 
-	try {
-		const group = await newGroup.save();
-		res.status(200).json({
-			body: {
-				id: group._id,
-				title: group.title,
-				themeColor: group.themeColor,
-				createdAt: group.createdAt,
-			},
-			message: `group created successfully`,
-		});
-	} catch (err) {
-		res.status(500).json({
-			err,
-		});
-	}
-});
+groups.delete('/removeGroup', removeGroup);
 
-groups.get('/getGroups', async (req: Request, res: Response) => {
-	const userId = getSessionUserId(req);
-
-	const groups = await Group.find({ userId });
-	try {
-		res.status(200).json({
-			body: groups,
-		});
-	} catch (err) {
-		res.status(500).json({
-			err,
-		});
-	}
-});
-
-groups.delete('/removeGroup', async (req: Request, res: Response) => {
-	const group = await Group.deleteOne({ _id: req.body._id });
-	try {
-		res.status(200).json({
-			body: group,
-		});
-	} catch (err) {
-		res.status(500).json({
-			err,
-		});
-	}
-});
-
-groups.patch('/editGroup', validateBody<CreateEditGroupType>(createEditGroupSchema), async (req: Request, res: Response) => {
-	await Group.updateOne({ _id: req.body._id }, { $set: { title: req.body.title } });
-	try {
-		res.status(200).json({
-			message: `status changed successfully to ${req.body.title}`,
-		});
-	} catch (err) {
-		res.status(500).json({
-			err,
-		});
-	}
-});
+groups.patch('/editGroup', validateBody<CreateEditGroupType>(createEditGroupSchema), editGroup);
 
 export default groups;
