f645083ea86fd334a2db0e7591ad99555830d000
Katarzyna Klimek <katarzynaklimek26@gmail.com>
2022-08-03 13:01:05 +0200

    mobile view fixes

--- client/src/components/Tasks/Tasks.tsx
+++ client/src/components/Tasks/Tasks.tsx
@@ -8,7 +8,7 @@ export const TasksList: FC = () => {
 	const { inCompletedTasks, completedTasks, isLoading } = useTasks();
 
 	return (
-		<div className='flex overflow-y-scroll overflow-x-hidden flex-1 flex-col'>
+		<div className='flex overflow-y-scroll overflow-x-hidden flex-1 flex-col max-h-[70vh]'>
 			<div>
 				{isLoading ? (
 					<Loader className='animate-spin m-auto' />
