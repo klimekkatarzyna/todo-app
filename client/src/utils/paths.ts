54c6ea043c5ccc3ece1f82a243a71bc31372e76f
Katarzyna Krawczyk <katarzynaklimek26@gmail.com>
2022-09-12 00:16:05 +0200

    eslint warning

--- client/src/utils/paths.ts
+++ client/src/utils/paths.ts
@@ -1,9 +1,9 @@
 import { ROUTE } from '../enums';
 
 export type ExtractRouteParams<T> = string extends T
-	? Record<string, string>
+	? Record<string, string> // eslint-disable-next-line @typescript-eslint/no-unused-vars
 	: T extends `${infer _Start}:${infer Param}/${infer Rest}`
-	? { [k in Param | keyof ExtractRouteParams<Rest>]: string }
+	? { [k in Param | keyof ExtractRouteParams<Rest>]: string } // eslint-disable-next-line @typescript-eslint/no-unused-vars
 	: T extends `${infer _Start}:${infer Param}`
 	? { [k in Param]: string }
 	: undefined;
